f9c6342c20ac29c62cd09ebb7a2f6574572ea31b
Adrien Thouvenin <adaura51@gmail.com>
2021-01-13 16:04:40 +0100

    fix: Translate French -> English

---
+++ migrations/Version20210113150249.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210113150249 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE tractor (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, license_plate VARCHAR(9) NOT NULL, purchase_date INT NOT NULL, year INT NOT NULL, consumption_fuel VARCHAR(255) NOT NULL, power VARCHAR(255) NOT NULL, brand VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, value INT NOT NULL, motor_load_coefficient INT NOT NULL, INDEX IDX_41F62668A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, company_name VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE tractor ADD CONSTRAINT FK_41F62668A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tractor DROP FOREIGN KEY FK_41F62668A76ED395');
+        $this->addSql('DROP TABLE tractor');
+        $this->addSql('DROP TABLE user');
+    }
+}
